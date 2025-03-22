943ec7cfa544df70c330dd49155ed241fec0ba24
iddev5 <ayushbardhan5@gmail.com>
2025-03-22 16:42:42 +0530

    Barebones header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,24 @@
+import Link from 'next/link';
+
 export default function Header() {
-    return <>
-        Header
-    </>
+    return <div className="w-screen flex justify-between px-22 py-6">
+        <p>LOGO</p>
+        <ul className="flex gap-10 text-lg">
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/events">Events</Link>
+            </li>
+            <li>
+                <Link href="/sponsors">Sponsors</Link>
+            </li>
+            <li>
+                <Link href="/speakers">Speakers</Link>
+            </li>
+            <li>
+                <Link href="/contact">Contact</Link>
+            </li>
+        </ul>
+    </div>
 }
