05af3b59ae93af797a0a24cc5d87502220251109
iddev5 <ayushbardhan5@gmail.com>
2025-03-26 00:02:44 +0530

    WIP Sponsor and speakers marquee

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,21 @@ function EventCard({logo, title, desc, reg_url, details_url}) {
   </div>
 }
 
+function SpeakerCard({photo, name, desig}) {
+  return <div className="flex flex-col items-center gap-2">
+    <div className="w-[150px] h-[150px] rounded-full bg-red-500"></div>
+    <h1 className="text-xl">{name}</h1>
+    <p className="text-sm">{desig}</p>
+  </div>
+}
+
+function SponsorCard({photo, name}) {
+  return <div className="flex flex-col items-center gap-2">
+    <div className="w-[150px] h-[150px] rounded-full bg-red-500"></div>
+    <h1 className="text-xl">{name}</h1>
+  </div>
+}
+
 export default function Home() {
   return (<>
     <div>Hero</div>
@@ -33,5 +48,37 @@ export default function Home() {
         )}
       </div>
     </div>
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl w-full text-center">PAST SPEAKERS</h1>
+      <div className="relative flex overflow-x-hidden w-screen gap-10 my-12">
+        <div className="flex gap-10 animate-marquee whitespace-nowrap">
+          {Array(8).fill(0).map((it, i) => 
+            <SpeakerCard name='Mr. Test' desig='Helping e-cell' />
+          )}
+        </div>
+        <div className="flex gap-10 animate-marquee whitespace-nowrap">
+          {Array(8).fill(0).map(it => 
+            <SpeakerCard name='Mr. Test' desig='Helping e-cell' />
+          )}
+        </div>
+      </div>
+    </div>
+
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl w-full text-center">PAST SPONSORS</h1>
+      <div className="relative flex overflow-x-hidden w-screen gap-10 my-12">
+        <div className="flex gap-10 animate-marquee2 whitespace-nowrap">
+          {Array(8).fill(0).map((it, i) => 
+            <SponsorCard name='Company X' />
+          )}
+        </div>
+        <div className="flex gap-10 animate-marquee2 whitespace-nowrap">
+          {Array(8).fill(0).map(it => 
+            <SponsorCard name='Company X' />
+          )}
+        </div>
+      </div>
+    </div>
+
   </>);
 }
