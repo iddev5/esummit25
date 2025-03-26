5b2fef8951685b72d6b552173d71e8947c374afc
iddev5 <ayushbardhan5@gmail.com>
2025-03-26 12:05:11 +0530

    Basic components for contact us page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,24 @@
+function Team({photo, name, post, x, fb, ln, ig}) {
+    return <div className="w-[200px] h-[200px] group hover:scale-115 transition rounded-md border-solid border-2 border-green-500">
+        <div className="p-4 w-full h-full bg-red-500 flex flex-col items-center justify-center">
+            <div className="bg-green-500 w-[150px] h-[150px] z-[100] group-hover:rounded-full rounded-lg"></div> {/* IMAGE */}
+            <h1 className="text-xl">{name}</h1>
+            <p className="opacity-0 absolute group-hover:relative group-hover:opacity-100 transition">{post}</p>
+        </div>
+    </div>
+}
+
 export default function Contact() {
-    return (<>Contact Us</>);
+    return (<>
+        <div className="flex flex-col items-center">
+            <h1 className="text-5xl w-full text-center">THE TEAM</h1>
+            <div className="flex flex-col w-[65vw] items-center gap-8 my-8 flex-wrap">
+                <h1 className="text-3xl uppercase">For any website related queries:</h1>
+                <div className="flex gap-10">
+                    <Team name="Someone" post="Tech Lead" />
+                    <Team name="Someone" post="Tech Lead" />
+                </div>
+            </div>
+        </div>
+    </>);
 }
