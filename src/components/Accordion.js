4982ef71dec3a6b8c8efbc0b4be4c044d54c8429
iddev5 <ayushbardhan5@gmail.com>
2025-03-26 12:33:14 +0530

    Event details page and Accordion element from old site

---
+++ src/components/Accordion.js
@@ -0,0 +1,51 @@
+"use client";
+import { useState } from "react";
+
+export default function Accordion({ title, answer }) {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  return (
+    <div className="w-full h-full py-2  border bg-black px-1 rounded decoration-0">
+      <button
+        onClick={() => setAccordionOpen(!accordionOpen)}
+        className="flex justify-between w-full p-1"
+      >
+        <span className="p-1 main-font text-white">{title}</span>
+        <svg
+          className="fill-indigo-800 shrink-0 ml-8"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center transition duration-200 ease-out ${
+              accordionOpen && "!rotate-180"
+            }`}
+          />
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+              accordionOpen && "!rotate-180"
+            }`}
+          />
+        </svg>
+      </button>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out decoration-0 text-slate-600 text-sm ${
+          accordionOpen
+            ? "grid-rows-[1fr] opacity-100 decoration-0"
+            : "grid-rows-[0fr] opacity-0 decoration-0"
+        }`}
+      >
+        <div className="overflow-hidden mt-1 decoration-0 main-font text-white">{answer}</div>
+      </div>
+    </div>
+  );
+};
