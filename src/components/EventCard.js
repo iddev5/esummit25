88e9c943daa92cd8cd52d81704b59aebd4670e57
iddev5 <ayushbardhan5@gmail.com>
2025-03-30 17:18:35 +0530

    Improve events card

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,27 +3,29 @@ import Image from "next/image";
 import styles from './event-card.module.css';
 
 export default function EventCard({ logo, title, desc, reg_url, details_url }) {
-  return <div className={styles['flip-card']}>
-    <div className={styles['flip-card-inner']}>
-      <div className={styles['flip-card-front']}>
-        <Image src={logo} alt="Event" width={300} height={300} />
-      </div>
-      <div className={styles["flip-card-back"]}>
-        <h1 className="uppercase font-semibold text-lg">{title}</h1>
-        <p>{desc}</p>
-        <div className="flex justify-between w-full text-center p-2 px-6 gap-2">
-          {[
-            <a href={reg_url} target="blank">
-              Register
-            </a>,
-            <Link href={details_url}>Event&nbsp;Details</Link>,
-          ].map((it) => (
-            <div className="w-[125px] bg-[#171717] p-2 rounded-lg cursor-pointer hover:scale-105 transition">
-              {it}
-            </div>
-          ))}
+  return <div className="flex flex-col items-center p-4 rounded-lg gap-2 border-1 border-solid border-[#ffd35b8e]">
+    <div className={styles['flip-card']}>
+      <div className={styles['flip-card-inner']}>
+        <div className={styles['flip-card-front']}>
+          <Image src={logo} alt="Event" width={300} height={300} />
+        </div>
+        <div className={styles["flip-card-back"]}>
+          <p>{desc}</p>
         </div>
       </div>
+    </div>
+    <h1 className="uppercase font-semibold text-lg">{title}</h1>
+    <div className="flex justify-around w-full text-center p-2 px-0 gap-2">
+      {[
+        <a href={reg_url} target="blank">
+          Register
+        </a>,
+        <Link href={details_url}>Event&nbsp;Details</Link>,
+      ].map((it) => (
+        <div className="w-[150px] bg-black p-2 px-4 text-center rounded-lg cursor-pointer hover:scale-105 transition">
+          {it}
+        </div>
+      ))}
     </div>
   </div>
 }
